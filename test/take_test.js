82ba526e54af2bb1073d9395f8518e1ffbf99a69
ellnix <ellnix@disroot.org>
2024-08-31 14:42:32 +0200

    Add tests for an array_take() method
    This seems to be a very popular method that I think we should add but I
    can't quite figure it out :(
    PRs welcome!

---
+++ test/take_test.js
@@ -0,0 +1,40 @@
+import assert from 'assert'
+import highdash from '../src/highdash.js'
+
+describe("highdash.array_take", () => {
+  it("takes elements from the front", () => {
+    let input_array = [1, 2, 3]
+    let n = 2
+
+    let result = highdash.array_take(input_array, n)
+
+    assert.deepEqual(result, [1, 2])
+  })
+
+  it("takes elements of any type", () => {
+    let input_array = ["hello", "world", "saturn", "jupiter"]
+    let n = 3
+
+    let result = highdash.array_take(input_array, n)
+
+    assert.deepEqual(result, ["hello", "world", "saturn"])
+  })
+
+  it("does not edit original array", () => {
+    let input_array = [1, 2, 3]
+    let n = 2
+
+    highdash.array_take(input_array, n)
+
+    assert.deepEqual(input_array, [1, 2, 3])
+  })
+
+  it("returns whole array if n is bigger than length", () => {
+    let input_array = [1, 2, 3]
+    let n = 4
+
+    const result = highdash.array_take(input_array, n)
+
+    assert.deepEqual(result, [1, 2, 3])
+  })
+})
