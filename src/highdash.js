eeabe775b90b11ce63ef3e6e24734125447d5494
ellnix <ellnix@disroot.org>
2024-09-04 16:53:03 +0200

    Clean up array_some comments

--- src/highdash.js
+++ src/highdash.js
@@ -33,13 +33,6 @@ highdash.array_filter = (array, predicate) => {
   return output;
 };
 
-/* 
-Logic of array_some function:
-The function checks if inside of the array there is at least one element that pass the condition
-*/
-
-const arrayForTest = [1, 2, 4, 5];
-
 highdash.array_some = (array, predicate) => {
   for (let i = 0; i < array.length; i++) {
     const returnValue = predicate(array[i]);
@@ -50,6 +43,5 @@ highdash.array_some = (array, predicate) => {
 
   return false;
 };
-/* array_some(arrayForTest, (x) => x % 3 === 0);
- */
+
 export default highdash;
