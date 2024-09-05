eeff74bf56b626b8b6f67f0273a27bb2a044e52c
duminore.dev <duminore.dev@gmail.com>
2024-09-05 15:54:30 +0200

    Implement array_product function and fix assertion

--- test/product_test.js
+++ test/product_test.js
@@ -1,20 +1,20 @@
-import assert from 'assert'
-import highdash from '../src/highdash.js'
+import assert from "assert";
+import highdash from "../src/highdash.js";
 
 describe("highdash.array_product", () => {
   it("returns the product of all the elements", () => {
-    const input_array = [2, 2, 3]
+    const input_array = [2, 2, 3];
 
-    const result = highdash.array_product(input_array)
+    const result = highdash.array_product(input_array);
 
-    assert.strictEqual(result, 12)
-  })
+    assert.strictEqual(result, 12);
+  });
 
   it("returns 1 if the array is empty", () => {
-    const input_array = []
+    const input_array = [];
 
-    const result = highdash.array_product(input_array)
+    const result = highdash.array_product(input_array);
 
-    assert.strictEqual(result, 0)
-  })
-})
+    assert.strictEqual(result, 1);
+  });
+});
